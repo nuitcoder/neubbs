0863ec000db00acd592e6a206bb8340f8d21bd7b
ahonn <ahonn95@outlook.com>
2017-10-13 11:32:11 +0800

    refactor: header component & global styling

--- src/main/webapp/app/App.jsx
+++ src/main/webapp/app/App.jsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { injectGlobal } from 'styled-components'
 
-import Header from './layouts/Header'
+import Header from './components/Header'
 
-const App = ({ children }) => (
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  body {
+    color: #333;
+    background-color: #e5e5e5;
+    font-size: 14px;
+    font-family: Helvetica, Arial, "PingFang SC", Roboto, "Microsoft Yahei", sans-serif;
+  }
+`
+
+const App = ({ children, router }) => (
   <div className="app">
-    <Header />
-    {children}
+    <Header router={router} />
+    <div id="main" className="container">
+      {children}
+    </div>
   </div>
-);
+)
 
 export default App
---
+++ src/main/webapp/app/components/Header.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { Navbar, Nav, NavItem } from 'react-bootstrap'
+
+const LOGIN_EVENT_KEY = 'LOGIN'
+const REGISTER_EVENT_KEY = 'REGISTER'
+
+const StyledNavbar = styled(Navbar)`
+  background-color: #fff;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
+`
+
+const StyledLogoLink = styled(Link)`
+  color: #dd4c4f !important;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleRightNavbar = this.handleRightNavbar.bind(this)
+  }
+
+  handleRightNavbar(eventKey) {
+    const { router } = this.props
+
+    switch (eventKey) {
+      case LOGIN_EVENT_KEY:
+        router.push('/account/register')
+        break
+      case REGISTER_EVENT_KEY:
+        router.push('/account/register')
+        break
+      default:
+    }
+  }
+
+  render() {
+    return (
+      <StyledNavbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <StyledLogoLink to="/">Neubbs</StyledLogoLink>
+          </Navbar.Brand>
+        </Navbar.Header>
+        {/* <Nav> */}
+          {/* <NavItem eventKey={1} href="#">Link</NavItem> */}
+          {/* <NavItem eventKey={2} href="#">Link</NavItem> */}
+        {/* </Nav> */}
+        <Nav
+          pullRight
+          onSelect={this.handleRightNavbar}
+        >
+          <NavItem eventKey={REGISTER_EVENT_KEY}>注册</NavItem>
+          <NavItem eventKey={LOGIN_EVENT_KEY}>登陆</NavItem>
+        </Nav>
+      </StyledNavbar>
+    )
+  }
+}
+
+Header.PropTypes = {
+  router: PropTypes.object.isRequired,
+}
+
+export default Header
