9b52e96a694ac5054a304fba6632eb74d8a50e5c
ahonn <ahonn95@outlook.com>
2017-10-14 11:59:55 +0800

    feat: add login form validate rules(username, password)

--- src/main/webapp/app/components/LoginForm.jsx
+++ src/main/webapp/app/components/LoginForm.jsx
@@ -2,29 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 import { Form, Field, reduxForm } from 'redux-form'
 import { FormGroup, FormControl, Button, Col } from 'react-bootstrap'
+import Validator from 'validatorjs'
 
 const Wrapper = styled(({ className, children }) => (
   <Col md={6} className={className}>
     {children}
   </Col>
 ))`
-float: none;
-margin: 10px auto 0;
+  float: none;
+  margin: 10px auto 0;
 `
 
 const FormHeader = styled.div`
-padding: 7px 15px;
-background-color: #fafafa;
-border-bottom: 1px solid #eee;
-border-top-left-radius: 4px;
-border-top-right-radius: 4px;
+  padding: 7px 15px;
+  background-color: #fafafa;
+  border-bottom: 1px solid #eee;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
 `
 
 const FormWrapper = styled.div`
-padding: 15px;
-background-color: #fff;
-border-bottom-left-radius: 4px;
-border-bottom-right-radius: 4px;
+  padding: 15px;
+  background-color: #fff;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
 `
 
 const InputError = styled.span`
@@ -36,16 +37,26 @@ const InputError = styled.span`
 `
 
 const validate = (values) => {
+  const rules = {
+    username: 'required|between:3,15',
+    password: 'required|between:6,16',
+  }
+  const messages = {
+    'required.username': '请输入用户名',
+    'between.username': '用户名应为 3 ～ 15 个英文字符',
+    'required.password': '请输入密码',
+    'between.password': '密码应为 6 ～ 16 个字符',
+  }
+  // validate form values
+  const validation = new Validator(values, rules, messages)
+
   const errors = {}
-  if (!values.username) {
-    errors.username = '请填写用户名'
+  // add errors messages when validate fails
+  if (validation.fails()) {
+    errors.username = validation.errors.first('username')
+    errors.password = validation.errors.first('password')
   }
 
-  if (!values.password) {
-    errors.password = '请填写密码'
-  }
-
-  console.log(errors)
   return errors
 }
 
