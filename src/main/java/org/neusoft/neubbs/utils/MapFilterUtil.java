2c2d5296df863e5e8f2ccd74c99d56a76ddf7afa
suvan <13202405189@163.com>
2017-11-23 18:53:56 +0800

    style: alter comment and cancel lastreplytime key-value format conversion

--- src/main/java/org/neusoft/neubbs/utils/MapFilterUtil.java
+++ src/main/java/org/neusoft/neubbs/utils/MapFilterUtil.java
@@ -3,7 +3,6 @@ package org.neusoft.neubbs.utils;
 
 import org.neusoft.neubbs.constant.api.ParamConst;
 
-import java.util.Calendar;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -50,15 +49,15 @@ public final class MapFilterUtil {
 
 
    /**
-    * 过滤用户信息（用于 api 页面显示 model）
+    * 过滤用户信息（account api 查询用户信息）
+    *    - name -> username
+    *    - 删除 id,name,password,image,rank,state,createtime
     *
     * @param userInfoMap 用户信息Map
     */
    public static void filterUserInfo(Map<String, Object> userInfoMap) {
-      //name -> username
       userInfoMap.put(ParamConst.USERNAME, userInfoMap.get(ParamConst.NAME));
 
-      //删除 id,name,password,image,rank,state,createtime
       removeKeys(userInfoMap,
               new String[] {
                   ParamConst.ID, ParamConst.NAME,
@@ -69,63 +68,51 @@ public final class MapFilterUtil {
 
    /**
     * 过滤话题用户信息（只保留 username 和 image）
+    *    - 只保留 name, image
+    *    - name -> username（删除 name）
     *
-    * @param userInfoMap
+    * @param userInfoMap 用户信息Map
     */
    public static void filterTopicUserInfo(Map<String, Object> userInfoMap) {
-      //只保留 name, image
       keepKesy(userInfoMap, new String[] {ParamConst.NAME, ParamConst.IMAGE});
 
-      //name -> username（删除 name）
       userInfoMap.put(ParamConst.USERNAME, userInfoMap.get(ParamConst.NAME));
       userInfoMap.remove(ParamConst.NAME);
    }
 
    /**
     * 过滤话题信息（针对 TopicDO， forum_topic）
+    *    - id -> topicid (删除 id)
+    *    - 删除 id, userid,
     *
     * @param topicInfoMap 话题信息 Map
     */
    public static void filterTopicInfo(Map<String, Object> topicInfoMap) {
-      // id -> topicid (删除 id)
       topicInfoMap.put(ParamConst.TOPIC_ID, topicInfoMap.get(ParamConst.ID));
 
-      //时间戳 -> xx天前 （传入：发表时间 + 当前时间）
-      String separateDay = StringUtil.getSeparateDay((long) topicInfoMap.get(ParamConst.LAST_REPLY_TIME),
-                                                         Calendar.getInstance().getTimeInMillis());
-
-      topicInfoMap.put(ParamConst.LAST_REPLY_TIME, separateDay);
-
-      //删除 userid,lastreplyuserid
-      removeKeys(topicInfoMap, new String[] {ParamConst.ID, ParamConst.USER_ID, ParamConst.LAST_REPLY_USER_ID});
+      removeKeys(topicInfoMap, new String[] {ParamConst.ID, ParamConst.USER_ID});
    }
 
    /**
     * 过滤话题内容信息（针对 TopicContentDO, forum_topic_content）
+    *    - 删除 id，topicid
     *
     * @param topicContentInfoMap 话题内容信息Map
     */
    public static void filterTopicContentInfo(Map<String, Object> topicContentInfoMap) {
-     //删除 id，topicid
       removeKeys(topicContentInfoMap, new String[] {ParamConst.ID, ParamConst.TOPIC_ID, ParamConst.CREATETIME});
    }
 
    /**
     * 过滤话题回复（针对 TopicReplyDO, forum_topic_reply）
+    *    - id -> replyId
+    *    - 删除（id，userid，topicid）
     *
     * @param topicReplyInfoMap 话题回复信息Map
     */
    public static void filterTopicReply(Map<String, Object> topicReplyInfoMap) {
-      // id -> replyId
       topicReplyInfoMap.put(ParamConst.REPLY_ID, topicReplyInfoMap.get(ParamConst.ID));
 
-      //时间戳 -> xx天前
-      String separateDay = StringUtil.getSeparateDay((long) topicReplyInfoMap.get(ParamConst.CREATETIME),
-              Calendar.getInstance().getTimeInMillis());
-
-      topicReplyInfoMap.put(ParamConst.CREATETIME, separateDay);
-
-      //是删除（id，userid，topicid）
       removeKeys(topicReplyInfoMap, new String[] {ParamConst.ID, ParamConst.USER_ID, ParamConst.TOPIC_ID});
    }
 
