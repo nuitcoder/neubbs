124aebaddee882354a36e9253aeb0d5b9dc1e5b4
suvan <13202405189@163.com>
2017-11-29 16:33:33 +0800

    config: alter neubbs.properties, add neubbs ftp cloud server information

--- src/main/java/org/neusoft/neubbs/entity/properties/NeubbsConfigDO.java
+++ src/main/java/org/neusoft/neubbs/entity/properties/NeubbsConfigDO.java
@@ -13,47 +13,80 @@ public class NeubbsConfigDO {
     private Integer topicsApiRequestParamLimitDefault;
     private String userImageUploadPath;
     private String accountApiVaslidateUrl;
-    private String userFtpUrl;
 
-    public Integer getCookieAutoLoginMaxAgeDay() {
-        //day -> second
-        return cookieAutoLoginMaxAgeDay * SetConst.TWENTY_FOUR * SetConst.SIXTY * SetConst.SIXTY;
-    }
+    private String ftpIp;
+    private Integer ftpPort;
+    private String ftpUsername;
+    private String ftpPassword;
 
+    /**
+     * Setter
+     */
     public void setCookieAutoLoginMaxAgeDay(Integer cookieAutoLoginMaxAgeDay) {
         this.cookieAutoLoginMaxAgeDay = cookieAutoLoginMaxAgeDay;
-    }
-
-    public Integer getTopicsApiRequestParamLimitDefault() {
-        return topicsApiRequestParamLimitDefault;
     }
 
     public void setTopicsApiRequestParamLimitDefault(Integer topicsApiRequestParamLimitDefault) {
         this.topicsApiRequestParamLimitDefault = topicsApiRequestParamLimitDefault;
     }
 
-    public String getUserImageUploadPath() {
-        return userImageUploadPath;
-    }
-
     public void setUserImageUploadPath(String userImageUploadPath) {
         this.userImageUploadPath = userImageUploadPath;
-    }
-
-    public String getAccountApiVaslidateUrl() {
-        return accountApiVaslidateUrl;
     }
 
     public void setAccountApiVaslidateUrl(String accountApiVaslidateUrl) {
         this.accountApiVaslidateUrl = accountApiVaslidateUrl;
     }
 
-    public String getUserFtpUrl() {
-        return userFtpUrl;
+    public void setFtpIp(String ftpIp) {
+        this.ftpIp = ftpIp;
     }
 
-    public void setUserFtpUrl(String userFtpUrl) {
-        this.userFtpUrl = userFtpUrl;
+    public void setFtpPort(Integer ftpPort) {
+        this.ftpPort = ftpPort;
     }
 
+    public void setFtpUsername(String ftpUsername) {
+        this.ftpUsername = ftpUsername;
+    }
+
+    public void setFtpPassword(String ftpPassword) {
+        this.ftpPassword = ftpPassword;
+    }
+
+    /**
+     * Getter
+     */
+    public Integer getCookieAutoLoginMaxAgeDay() {
+        //day -> second
+        return cookieAutoLoginMaxAgeDay * SetConst.TWENTY_FOUR * SetConst.SIXTY * SetConst.SIXTY;
+    }
+
+    public Integer getTopicsApiRequestParamLimitDefault() {
+        return topicsApiRequestParamLimitDefault;
+    }
+
+    public String getUserImageUploadPath() {
+        return userImageUploadPath;
+    }
+
+    public String getAccountApiVaslidateUrl() {
+        return accountApiVaslidateUrl;
+    }
+
+    public String getFtpIp() {
+        return ftpIp;
+    }
+
+    public Integer getFtpPort() {
+        return ftpPort;
+    }
+
+    public String getFtpUsername() {
+        return ftpUsername;
+    }
+
+    public String getFtpPassword() {
+        return ftpPassword;
+    }
 }
