a20a81ac51b4c92139749f6025921ba2d71330d8
suvan <13202405189@163.com>
2017-12-14 17:56:02 +0800

    sql: design forum_user_dynamic, forum_topic_action, forum_message table, add initBuildForumTables.sql

--- src/main/sql/initBuildForumDataTables.sql
+++ src/main/sql/initBuildForumDataTables.sql
@@ -25,7 +25,11 @@ DROP TABLE IF EXISTS `forum_topic_content`;
 DROP TABLE IF EXISTS `forum_topic_reply`;
 
 
--- 论坛用户
+-- ----------------------------
+-- 论坛用户表
+-- ----------------------------
+
+-- 用户信息
 CREATE TABLE `forum_user` (
   `fu_id` INT(11) NOT NULL AUTO_INCREMENT comment '用户 id',
   `fu_name` VARCHAR(15) DEFAULT NULL comment '用户名',
@@ -44,18 +48,41 @@ CREATE TABLE `forum_user` (
    UNIQUE KEY `fu_email` (`fu_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
-
--- 论坛用户行为
+-- 用户行为
 CREATE TABLE `forum_user_action` (
-  `fua_id` INT(11) NOT NULL AUTO_INCREMENT comment '操作 id',
+  `fua_id` INT(11) NOT NULL AUTO_INCREMENT comment '用户操作 id',
   `fu_id` INT(11) NOT NULL comment '用户 id',
-  `fua_liketopic_ft_id_array` JSON comment 'JSON 格式，话题 id, 喜欢话题数组',
+  `fua_liketopic_ft_id_array` JSON comment '用户喜欢所有话题 id 数组（JSON int）',
    PRIMARY KEY (`fua_id`),
    CONSTRAINT `FUA_FU_ID` FOREIGN KEY (`fu_id`) REFERENCES `forum_user` (`fu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
+-- 用户动态
+CREATE TABLE `forum_user_dynamic` (
+  `fud_id` INT(11) NOT NULL AUTO_INCREMENT comment '用户动态 id',
+  `fu_id` INT(11) NOT NULL comment '用户 id',
+  `fud_information` JSON comment '用户动态信息（JSON{{"发布时间","消息"},{ ... }}）',
+  PRIMARY KEY (`fud_id`),
+  CONSTRAINT `FUD_FU_ID` FOREIGN KEY (`fu_id`) REFERENCES `forum_user` (`fu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
--- 论坛话题
+
+-- ----------------------------
+-- 论坛话题表
+-- ----------------------------
+
+-- 话题分类（由管理员定义）
+CREATE TABLE `forum_topic_category` (
+  `ftcg_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题分类id',
+  `ftcg_nick` VARCHAR(100) DEFAULT NULL comment '分类昵称（全英文）',
+  `ftcg_name` VARCHAR(100) DEFAULT NULL comment '分类名字',
+  `ftcg_description` VARCHAR(150) DEFAULT 'no' comment '分类描述',
+  PRIMARY KEY (`ftcg_id`),
+  UNIQUE KEY `ftcg_nick` (`ftcg_nick`),
+  UNIQUE KEY `ftcg_name` (`ftcg_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
+
+-- 话题基本信息
 CREATE TABLE `forum_topic` (
   `ft_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题 id',
   `fu_id` INT(11) NOT NULL comment '发表人 id （对应 forum_user）',
@@ -71,8 +98,7 @@ CREATE TABLE `forum_topic` (
   CONSTRAINT `FT_L_FU_ID` FOREIGN KEY (`ft_lastreplyuserid`) REFERENCES `forum_user` (`fu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
-
--- 论坛话题内容
+-- 话题内容
 CREATE TABLE `forum_topic_content` (
   `ftc_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题内容 id',
   `ft_id` INT(11) NOT NULL UNIQUE comment '对应话题 id （对应 forum_topic）',
@@ -83,10 +109,19 @@ CREATE TABLE `forum_topic_content` (
   CONSTRAINT `FTC_FT_ID` FOREIGN KEY (`ft_id`) REFERENCES `forum_topic` (`ft_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
+-- 话题行为
+CREATE TABLE `forum_topic_action` (
+  `fta_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题行为 id',
+  `ft_id` INT(11) NOT NULL UNIQUE comment '话题 id',
+  `fta_replys_fu_id_array` JSON comment '话题所有回复人 id 数组（JSON int）',
+  `fta_liketopic_fu_id_array` JSON comment '所有喜欢话题用户 id 数组（JSON int）',
+  PRIMARY KEY (`fta_id`),
+  CONSTRAINT `FTA_FT_ID` FOREIGN KEY (`ft_id`) REFERENCES `forum_topic` (`ft_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
--- 论坛话题回复
+-- 话题回复
 CREATE TABLE `forum_topic_reply` (
-  `ftr_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题回复id',
+  `ftr_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题回复 id',
   `fu_id` INT(11) DEFAULT NULL comment '回复人 id',
   `ft_id` INT(11) DEFAULT NULL comment '对应话题 id',
   `ftr_content` VARCHAR(150) NOT NULL comment '话题内容',
@@ -99,13 +134,20 @@ CREATE TABLE `forum_topic_reply` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
 
 
--- 论坛话题分类
-CREATE TABLE `forum_topic_category` (
-  `ftcg_id` INT(11) NOT NULL AUTO_INCREMENT comment '话题分类id',
-  `ftcg_nick` VARCHAR(100) DEFAULT NULL comment '分类昵称（全英文）',
-  `ftcg_name` VARCHAR(100) DEFAULT NULL comment '分类名字',
-  `ftcg_description` VARCHAR(150) DEFAULT 'no' comment '分类描述',
-  PRIMARY KEY (`ftcg_id`),
-  UNIQUE KEY `ftcg_nick` (`ftcg_nick`),
-  UNIQUE KEY `ftcg_name` (`ftcg_name`)
+-- ----------------------------
+-- 论坛消息表（消息通知）
+-- ----------------------------
+
+-- 消息记录
+CREATE TABLE `forum_message` (
+  `fm_id` INT(11) NOT NULL AUTO_INCREMENT comment '消息 id',
+  `fm_source` VARCHAR(20) NOT NULL comment '消息来源',
+  `fm_sender_id` INT(11) NOT NULL comment '发送人 id',
+  `fm_send_time` DATETIME DEFAULT CURRENT_TIMESTAMP comment '发送时间（默认为创建时间）',
+  `fm_content` VARCHAR(500) NOT NULL comment '消息内容',
+  `fm_recevier_id` INT(11) NOT NULL comment '接收人 id',
+  `fm_receive_time` DATETIME DEFAULT NULL comment '接收时间（默认空）',
+  PRIMARY KEY (`fm_id`),
+  CONSTRAINT `FM_S_FU_ID` FOREIGN KEY (`fm_sender_id`) REFERENCES `forum_user` (`fu_id`),
+  CONSTRAINT `FM_R_FU_ID` FOREIGN KEY (`fm_recevier_id`) REFERENCES `forum_user` (`fu_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_bin;
