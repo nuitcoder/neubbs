b90ee86d4b52649de244c12a0a202fe97131fa28
suvan <13202405189@163.com>
2017-12-15 22:52:01 +0800

    feat: add forum_message domain object, data access object, mapper.xml

---
+++ src/main/java/org/neusoft/neubbs/dao/IMessageDAO.java
@@ -0,0 +1,42 @@
+package org.neusoft.neubbs.dao;
+
+import org.neusoft.neubbs.entity.MessageDO;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 信息数据访问接口
+ *      - 针对 forum_message
+ *      - resources/mapping/MessageMapper.xml 配置 SQL
+ *
+ * @author Suvan
+ */
+@Repository
+public interface IMessageDAO {
+
+    /**
+     * 保存消息
+     *      - 消息对象需包含（消息来源，发送人 id，发送内容，回复人 id）
+     *      - 新生成的消息，自增 id 会重新注入传入的 MessageDO 对象
+     *
+     * @param message 消息对象
+     * @return int 插入行数
+     */
+    int saveMessage(MessageDO message);
+
+    /**
+     * （id）获取消息对象
+     *
+     * @param messageId 消息id
+     * @return MessageDO消息对象
+     */
+    MessageDO getMessage(int messageId);
+
+    /**
+     * 更新消息接收时间（回复人接收）
+     *      - MySQL 自动更新为当前时间
+     *
+     * @param messageId 消息id
+     * @return int 更新行数
+     */
+    int updateMessageReceiveTime(int messageId);
+}
---
+++ src/main/java/org/neusoft/neubbs/entity/MessageDO.java
@@ -0,0 +1,91 @@
+package org.neusoft.neubbs.entity;
+
+import java.util.Date;
+
+/**
+ * forum_message 领域模型
+ *
+ * @author Suvan
+ */
+public class MessageDO {
+
+    private Integer id;
+    private String source;
+
+    private Integer senderId;
+    private Date sendTime;
+    private String content;
+    private Integer receiverId;
+    private Date receiveTime;
+
+    /**
+     * Getter
+     */
+    public Integer getId() {
+        return id;
+    }
+    public String getSource() {
+        return source;
+    }
+    public Integer getSenderId() {
+        return senderId;
+    }
+    public Date getSendTime() {
+        return sendTime;
+    }
+    public String getContent() {
+        return content;
+    }
+    public Integer getReceiverId() {
+        return receiverId;
+    }
+    public Date getReceiveTime() {
+        return receiveTime;
+    }
+
+    /**
+     * Setter
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setSource(String source) {
+        this.source = source;
+    }
+    public void setSenderId(Integer senderId) {
+        this.senderId = senderId;
+    }
+    public void setSendTime(Date sendTime) {
+        this.sendTime = sendTime;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public void setReceiverId(Integer receiverId) {
+        this.receiverId = receiverId;
+    }
+    public void setReceiveTime(Date receiveTime) {
+        this.receiveTime = receiveTime;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("{");
+        sb.append("\"id\":")
+                .append(id);
+        sb.append(",\"source\":\"")
+                .append(source).append('\"');
+        sb.append(",\"senderId\":")
+                .append(senderId);
+        sb.append(",\"sendTime\":\"")
+                .append(sendTime).append('\"');
+        sb.append(",\"content\":\"")
+                .append(content).append('\"');
+        sb.append(",\"receiverId\":")
+                .append(receiverId);
+        sb.append(",\"receiveTime\":\"")
+                .append(receiveTime).append('\"');
+        sb.append('}');
+        return sb.toString();
+    }
+}
