985595a760d33755d5d652aa44179a0476032e66
Suvan-L“ <liushuwei0925@gmail.com>
2018-01-09 18:33:28 +0800

    fix: modify api filter, will http request and response to save 'PublicParamsUtil', each thread is independent

--- src/main/java/org/neusoft/neubbs/controller/filter/ApiFilter.java
+++ src/main/java/org/neusoft/neubbs/controller/filter/ApiFilter.java
@@ -1,5 +1,7 @@
 package org.neusoft.neubbs.controller.filter;
 
+import org.neusoft.neubbs.utils.PublicParamsUtil;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -23,25 +25,20 @@ public class ApiFilter implements Filter {
     @Override
     public void destroy() { }
 
-    /**
-     * 执行过滤器
-     *
-     * @param req servlet请求
-     * @param resp servlet响应
-     * @param chain 过滤链
-     * @throws ServletException Servlet异常
-     * @throws IOException IO异常
-     */
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
             throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        //访问api，即创建 session(用于统计在线人数)
-        request.getSession(true);
+        //set thread local param
+        PublicParamsUtil.setRequest(request);
+        PublicParamsUtil.setResponse(response);
 
-        //过滤器放行
+        //pass filter, http to the chain
         chain.doFilter(request, response);
+
+        //clear thread local param information
+        PublicParamsUtil.clearAllInfo();
     }
 }
