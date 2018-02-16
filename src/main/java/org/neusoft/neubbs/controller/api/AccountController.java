43975219df164c028ad6b16472deed8e1381ec38
Suvan-L“ <liushuwei0925@gmail.com>
2018-02-16 22:37:14 +0800

    fix: repair 'api/account/following' and 'api/account/followed', valicate service check() input type (userid -> id)

--- src/main/java/org/neusoft/neubbs/controller/api/AccountController.java
+++ src/main/java/org/neusoft/neubbs/controller/api/AccountController.java
@@ -131,7 +131,7 @@ public final class AccountController {
      */
     @RequestMapping(value = "/following", method = RequestMethod.GET)
     public ApiJsonDTO listUserFollowingUsersInfo(@RequestParam(value = "userid", required = false) String userId) {
-        validationService.check(ParamConst.USER_ID, userId);
+        validationService.check(ParamConst.ID, userId);
         return new ApiJsonDTO().success().model(userService.listAllFollowingUserInfoModelList(Integer.valueOf(userId)));
     }
 
@@ -143,7 +143,7 @@ public final class AccountController {
      */
     @RequestMapping(value = "/followed", method = RequestMethod.GET)
     public ApiJsonDTO listUserFollowedUsersInfo(@RequestParam(value = "userid", required = false) String userId) {
-        validationService.check(ParamConst.USER_ID, userId);
+        validationService.check(ParamConst.ID, userId);
         return new ApiJsonDTO().success().model(userService.listAllFollowedUserInfoModelList(Integer.valueOf(userId)));
     }
 
